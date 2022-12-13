f8274f449c6ddc7a659757a4e90a8b9352644ea4
Philip Nguyen <philipnguyen953@gmail.com>
2022-12-13 13:23:38 -0800

    Added sanitization/validation and passportjs

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const express = require("express");
 const mongoose = require('mongoose');
 const path = require('path');
+const bodyParser = require('body-parser')
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 // Require user/message models
 const User = require('./models/user');
@@ -17,11 +21,19 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 const app = express();
 const PORT = 3000;
 
+// Sets up View Engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, "./views"))
 
 // Sets up express middleware
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }));
+
+// Sets up passport middleware
+app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Sets up routes
 const indexRouter = require('./routes/index')
--- controllers/postSignUpController.js
+++ controllers/postSignUpController.js
@@ -1,19 +1,57 @@
 const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
 
-const postSignUpController = (req, res) => {
-    const user = new User({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        username: req.body.username,
-        password: req.body.password,
-        membership: true
-      })
-      user.save((err) => {
-        if (err) {
-            return next(err);
-        }
-      });
-      res.redirect("/")
-}
+const postSignUpController = (req, res, next) => {
+    // Sanitize form inputs
+    body("first_name", "First Name should not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape();
+
+    body("last_name", "Last Name should not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape();
+
+    body("username", "Username should not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape();
+
+    body("password", "Password should not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape();
+
+    body("confirm_password", "Password confirmation should not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape();
+
+    // Process errors after sanitization/validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return console.log(errors);
+    }
+
+    if (req.body.password === req.body.confirm_password) {
+        const user = new User({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            username: req.body.username,
+            password: req.body.password,
+            membership: false,
+        });
+        user.save((err) => {
+            if (err) {
+                return next(err);
+            }
+        });
+        res.redirect("/");
+    } else {
+        res.status(403).send("Passwords do not match...");
+        next(err);
+    }
+};
 
 module.exports = postSignUpController;
