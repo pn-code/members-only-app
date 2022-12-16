43150b5c17729d349e5fced6457d0b23506ae4a6
Philip Nguyen <philipnguyen953@gmail.com>
2022-12-15 19:04:06 -0800

    Added luxon to format dates

--- models/message.js
+++ models/message.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
 const { DateTime } = require("luxon");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
     title: { type: String, required: true, maxLength: 100 },
     text: { type: String, required: true, maxLength: 200 },
     timestamp: { type: Date, required: true },
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    author_name: { type: String, required: true }
-})
+    author_name: { type: String, required: true },
+});
+
+MessageSchema.virtual("formatted_date").get(function () {
+    return DateTime.fromJSDate(this.timestamp).toLocaleString(
+        DateTime.DATE_MED
+    );
+});
 
 module.exports = mongoose.model("Message", MessageSchema);
